71dd5e2219cf9536eb2e2cc68c2e3529f46d4ba0
paesall <anasalifaizal5@gmail.com>
2024-05-30 17:10:50 +0700

    e

---
+++ vue-project-praktikum8/src/components/ArtikelHome.vue
@@ -0,0 +1,11 @@
+<template>
+<div class="div">
+    <img src="../assets/pynt.jpg" alt="" style="height: 300px;">
+    <h1>ini adalah pak prof yanto</h1>
+    <p>berapi amikom ku</p>
+    <p>berapi amikom ku</p>
+    <p>berapi amikom ku</p>
+    <p>berapi amikom ku</p>
+    
+</div>
+</template>
